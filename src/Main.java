4c6f959728280e8a68eddb5ca2e8403b8b68d1d1
Clarence Martin <marticla000@warren.k12.in.us>
2025-02-10 12:59:40 -0500

    blah blah

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Tyrannosaurus");
+        arrayList.add("Tarbosaurus");
+        arrayList.add("Dasplateosaurus");
+        arrayList.add("Albertosaurus");
+        arrayList.add("Gorgosaurus");
+        System.out.println("Tyrannosauridae: " + arrayList);
+        arrayList.remove("Dasplateosaurus");
+
+        arrayList.sort(null);
+        System.out.println("(Updated)Tyrannosauridae: " + arrayList);
+    }
 }
